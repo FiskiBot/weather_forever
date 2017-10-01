b8625e2a6bc00c0c894d79436a0733cde8ec78cc
Patrick Moening <FiskiMac@mac-of-destiny.local>
2017-09-30 21:37:46 -0700

    First Commit

--- Weather Forever/DataService.swift
+++ Weather Forever/DataService.swift
@@ -9,13 +9,16 @@
 import Foundation
 
 class DataService : NSObject {
+    typealias DownloadCompete = () -> ()
     let URL_BASE = "https://api.openweathermap.org/data/2.5/weather?q="
     let IMPERIAL_UNITS = "units=imperial"
     let API_KEY = "APPID=1636b7bc68a4e607f7531309c857e09b"
     var weatherIcon = ""
+    var temp = ""
     static var ds = DataService()
     
-    func getWeather(city: String){
+    func getWeather(city: String, completion: DownloadCompete){
+        let tempChanged = Notification(name: Notification.Name(rawValue: "Temp Changed"), object: nil, userInfo: nil)
         if let URL = URL(string: "\(URL_BASE)\(city)&\(IMPERIAL_UNITS)&\(API_KEY)"){
             URLSession(configuration: .default).dataTask(with: URL, completionHandler: { (data, response, error) in
                 if let error = error {
@@ -24,9 +27,22 @@ class DataService : NSObject {
                     print("###########")
                 } else if let response = response, let data = data {
                     do {
-                        let jsonResult = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? [[String:AnyObject]]
+                        let jsonResult = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? [String:AnyObject]
+                        if let main = jsonResult!["main"] as? Dictionary<String,AnyObject>{
+                            if let getTemp = main["temp"] {
+                                self.temp = "\(getTemp)"
+                                NotificationCenter.default.post(tempChanged)
+                            }
+                        }
+                        if let weather = jsonResult!["weather"] as? [Dictionary<String,AnyObject>]{
+                            if let getWeather = weather[0]["icon"] {
+                                self.weatherIcon = "\(getWeather)"
+                                NotificationCenter.default.post(tempChanged)
+                            }
+                        }
+                        
                         print(jsonResult)
-                        print(response)
+                        //print(response)
                     } catch {
                         print("Json Serialization failed...")
                     }
--- Weather Forever/ViewController.swift
+++ Weather Forever/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreLocation
 class ViewController: UIViewController {
+    
     @IBOutlet weak var cityLbl: UILabel!
     @IBOutlet weak var forcastImg: UIImageView!
     @IBOutlet weak var fahrenheitLbl: UILabel!
@@ -19,11 +20,45 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-      getLocation()
+      //191, 251, 215 day
+    // 15 / 12 / 47
+    }
+    func updateUI() {
+        if DataService.ds.temp != "" {
+            let farenheitVal = round(Double(DataService.ds.temp)!)
+            let celciusVal = round((farenheitVal - 32) * 5 / 9)
+            let downloadedImg = DataService.ds.weatherIcon
+            DispatchQueue.main.async {
+                self.fahrenheitLbl.text = "\(farenheitVal)°F"
+                self.celciusLbl.text = "\(celciusVal)°C"
+                self.forcastImg.image = UIImage(named: downloadedImg)
+                
+                if downloadedImg.lowercased().characters.contains("n") {
+                    self.view.backgroundColor = UIColor(red: 15/255, green: 12/255, blue: 47/255, alpha: 1.0)
+                    self.cityLbl.textColor = UIColor.white
+                    self.fahrenheitLbl.textColor = UIColor.white
+                    self.celciusLbl.textColor = UIColor.white
+                    
+                } else {
+                    self.view.backgroundColor = UIColor(red: 191/255, green: 251/255, blue: 215/255, alpha: 1.0)
+                    self.cityLbl.textColor = UIColor.black
+                    self.fahrenheitLbl.textColor = UIColor.black
+                    self.celciusLbl.textColor = UIColor.black
+                    
+                }
+            }
+        }
     }
     
-    @IBAction func testJSon(_ sender: UIButton) {
-        DataService.ds.getWeather(city: cityLbl.text!)
+    override func viewDidAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector (updateUI), name: Notification.Name(rawValue: "Temp Changed"), object: nil)
+        
+        getLocation()
+        
+        let parsedCity = cityLbl.text?.replacingOccurrences(of: " ", with: "_")
+        DataService.ds.getWeather(city: parsedCity!, completion: {
+            
+        })
     }
 }
 
@@ -35,6 +70,24 @@ extension ViewController : CLLocationManagerDelegate {
             self.locManager.requestWhenInUseAuthorization()
             self.locManager.startUpdatingLocation()
             print(CLLocationManager.authorizationStatus().rawValue)
+        } else if CLLocationManager.authorizationStatus() == .restricted {
+            let alert = UIAlertController(title: "Location Not Authorized", message: "Please turn on location services in the settings", preferredStyle: .alert)
+            let settingsAction = UIAlertAction(title: "Settings", style: .default) { (_) -> Void in
+                guard let settingsUrl = URL(string: UIApplicationOpenSettingsURLString) else {
+                    return
+                }
+                
+                if UIApplication.shared.canOpenURL(settingsUrl) {
+                    UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                        print("Settings opened: \(success)") // Prints true
+                    })
+                }
+            }
+            alert.addAction(settingsAction)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+            alert.addAction(cancelAction)
+            
+            present(alert, animated: true, completion: nil)
         }
         let geoCoder = CLGeocoder()
         
