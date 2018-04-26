978ef43fe26d04fa848bd27a77ca0ff38b3242c3
Patrick Moening <FiskiMac@Mac-of-Destiny.local>
2018-04-25 22:33:09 -0700

    push to github

--- Weather Forever/DataService.swift
+++ Weather Forever/DataService.swift
@@ -12,7 +12,7 @@ class DataService : NSObject {
     typealias DownloadCompete = () -> ()
     let URL_BASE = "https://api.openweathermap.org/data/2.5/weather?q="
     let IMPERIAL_UNITS = "units=imperial"
-    let API_KEY = "APPID=1636b7bc68a4e607f7531309c857e09b"
+    let API_KEY = "" //Omited for github
     let cityKey = "cityArray"
     let defaultCities = ["Los Angeles", "New York", "Chicago", "Miami"]
     var selectedCity = ""
